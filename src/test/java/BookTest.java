7154d1eb7cdb1eb392deac51c8a9b613d177bfb6
mtomberger <mtomberger@lukmann.at>
2018-09-29 14:58:25 +0200

    persistence.xml setup

---
+++ src/test/java/BookTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+
+public class BookTest {
+    private static EntityManager _entityManager;
+
+    @Test
+    public void setup(){
+
+    }
+}
