5e2172e49f6b78df04b02fd9778d753084605628
mtomberger <mtomberger@lukmann.at>
2018-10-13 13:40:17 +0200

    sequence

--- src/main/db-scripts/SCRIPTS_II.sql
+++ src/main/db-scripts/SCRIPTS_II.sql
@@ -1,5 +1,7 @@
 ALTER TABLE buch RENAME COLUMN genre TO category;
-
+ALTER TABLE author DROP CONSTRAINT pk_author;
+DROP TABLE author
+CREATE SEQUENCE author_ID_Seq START WITH 101 INCREMENT BY 1;
 CREATE TABLE author (
    id INTEGER,
    firstname VARCHAR(75),
